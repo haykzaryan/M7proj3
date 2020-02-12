6ef8c296440efbfb56fd534f962d8244df1eb6dd
haykzaryan <hayk.zaryan@gmail.com>
2020-02-12 17:22:09 +0100

    4th

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-read web
+echo Website: ; read web
 
 docker run  --net=host -v /etc/localtime:/etc/localtime:ro -v $HOME/Education/SMX2/M7/proj3/datamailserver:/data --name "mailserverjonahayk" -h "$web" -t analogic/poste.io
