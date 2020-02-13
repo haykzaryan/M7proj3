a961f0dfc5705db734a1cf7dee9d12a849f22990
haykzaryan <hayk.zaryan@gmail.com>
2020-02-13 19:24:01 +0100

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-echo Website: ; read web
+echo Website1: ; read web1
+echo Website2: ; read web2
 
-docker run  --net=host -v /etc/localtime:/etc/localtime:ro -v $HOME/Education/SMX2/M7/proj3/datamailserver:/data --name "mailserverjonahayk" -h "$web" -t analogic/poste.io
+docker run  --net=host -v /etc/localtime:/etc/localtime:ro -v $HOME/Education/SMX2/M7/proj3/datamailserver:/data --name "mailserverjonahayk" -h "$web1" -h "$web2" -t analogic/poste.io
